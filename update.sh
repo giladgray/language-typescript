e2dcce4b32c481318096f761c5a2ce4fc70bb96f
Gilad Gray <ggray@palantir.com>
2017-03-15 14:13:39 -0700

    nvm, just copy-paste
    curl was losing a level of backslash escaping so the grammars wouldn't compile. removing the upgrade script, doing this by hand.

--- update.sh
+++
@@ -1,8 +0,0 @@
-BASE_PATH="https://raw.githubusercontent.com/TypeStrong/atom-typescript/master/grammars"
-TS=$(curl $BASE_PATH/ts.cson)
-TSX=$(curl $BASE_PATH/tsx.cson)
-
-ATTRIB="# Copied from https://github.com/TypeStrong/atom-typescript"
-
-echo "${ATTRIB}\n${TS}" > ./grammars/ts.cson
-echo "${ATTRIB}\n${TSX}" > ./grammars/tsx.cson
